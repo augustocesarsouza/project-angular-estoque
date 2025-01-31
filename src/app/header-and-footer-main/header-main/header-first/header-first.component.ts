8076b7eefd659ceef8ddb6d425098d9cf20151db
augustocesarsouza <augustocesarsantana90@gmail.com>
2025-01-31 14:00:57 -0400

    algumas alterações fazendo a funcionalidade de passar os spans

--- src/app/header-and-footer-main/header-main/header-first/header-first.component.ts
+++ src/app/header-and-footer-main/header-main/header-first/header-first.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, NgZone, OnDestroy, QueryList, ViewChild, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-header-first',
   templateUrl: './header-first.component.html',
   styleUrl: './header-first.component.scss'
 })
-export class HeaderFirstComponent {
+export class HeaderFirstComponent implements AfterViewInit, OnDestroy {
+  @ViewChildren('spansAdShowUser') spansAdShowUser!: QueryList<ElementRef<HTMLSpanElement>>;
+  @ViewChild('containerAllSpans') containerAllSpans!: ElementRef<HTMLDivElement>;
 
+  whichSpanShow = 0;
+  intervalId!: number;
+
+  constructor(private ngZone: NgZone){}
+
+  ngAfterViewInit(): void {
+    this.ngZone.runOutsideAngular(() => this.startRotation());
+  }
+
+  startRotation(): void {
+    if(typeof window === 'undefined')return;
+
+    const container = this.containerAllSpans.nativeElement as HTMLElement;
+    if (!container) return;
+
+    this.intervalId = window.setInterval(() => {
+      const firstSpan = container.firstElementChild as HTMLElement;
+
+      container.style.transition = "transform 1s ease-in-out";
+      container.style.transform = "translateX(-560px)";
+
+      setTimeout(() => {
+        container.appendChild(firstSpan); // Move o primeiro para o final
+        container.style.transition = "none"; // Remove transição para resetar posição
+        container.style.transform = "translateX(0)"; // Reseta posição
+      }, 1000);
+    }, 2000);
+  }
+
+  ngOnDestroy(): void {
+    clearTimeout(this.intervalId);
+  }
 }
--- src/app/header-and-footer-main/header-main/header-second/header-second.component.ts
+++ src/app/header-and-footer-main/header-main/header-second/header-second.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class HeaderSecondComponent {
 
+  onClickAccount(){
+    console.log('Account clicked');
+  }
+
+  mouseEnterAccount(){
+    console.log('mouseEnterAccount');
+  }
+
+  mouseLeaveAccount(){
+    console.log('mouseLeaveAccount');
+  }
 }
+
+// position: absolute; -- ISSO AQUI É DO CONTAINER FLUTUANDO Account
+// left: -100px;
+// top: 6px;
+// min-height: 100px;
+// width: 260px;
+// padding: 25px 20px 20px;
+// box-sizing: border-box;
+// background: #fff;
+// border: 1.5px solid #ddd;
+// box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+// overflow: visible;
+// z-index: 2;
+// text-align: left;

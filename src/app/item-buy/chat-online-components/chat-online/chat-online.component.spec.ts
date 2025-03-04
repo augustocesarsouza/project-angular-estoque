972397a53be52ab984ce6124b8e89f63c600a845
augustocesarsouza <augustocesarsantana90@gmail.com>
2025-03-04 12:27:53 -0400

    terminei de fazer chat online

--- src/app/item-buy/chat-online-components/chat-online/chat-online.component.spec.ts
+++ src/app/item-buy/chat-online-components/chat-online/chat-online.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ChatOnlineComponent } from './chat-online.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { AllSvgModule } from '../../../all-svg/all-svg.module';
 
 describe('ChatOnlineComponent', () => {
   let component: ChatOnlineComponent;
@@ -8,16 +11,51 @@ describe('ChatOnlineComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [AllSvgModule, HttpClientTestingModule, RouterTestingModule],
       declarations: [ChatOnlineComponent]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(ChatOnlineComponent);
     component = fixture.componentInstance;
+
+    component.getContainerChatOnline = jasmine.createSpy('getContainerChatOnline');
+    component.getContainerChatOnlineMinimize = jasmine.createSpy('getContainerChatOnlineMinimize');
+    component.getContainerChatOnline = jasmine.createSpy('getContainerChatOnline');
+    component.containerModalChatOnline = document.createElement('div');
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render svg message', () => {
+    const svg = fixture.nativeElement.querySelector('.container-chat-online-minimize > app-svg-message');
+    expect(svg).not.toBeNull();
+  });
+
+  it('should render span CHAT ONLINE', () => {
+    const span = fixture.nativeElement.querySelector('.container-chat-online-minimize > span');
+    expect(span.textContent.trim()).toBe("CHAT ONLINE");
+  });
+
+  it('should render chat online official svg message', () => {
+    const svg = fixture.nativeElement.querySelector('.container-chat-online > app-svg-message');
+    expect(svg).not.toBeNull();
+  });
+
+  it('should render span CHAT ONLINE', () => {
+    const span = fixture.nativeElement.querySelector('.container-chat-online > span');
+    expect(span.textContent.trim()).toBe("CHAT ONLINE");
+  });
+
+  it('should render span loading', () => {
+    component.firstClickMessage = true;
+    fixture.detectChanges();
+
+    const span = fixture.nativeElement.querySelector('.container-chat-online > span');
+    expect(span.textContent.trim()).toBe("CARREGANDO...");
   });
 });
--- src/app/item-buy/chat-online-components/modal-chat-online/modal-chat-online.component.spec.ts
+++ src/app/item-buy/chat-online-components/modal-chat-online/modal-chat-online.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ModalChatOnlineComponent } from './modal-chat-online.component';
+import { ElementRef } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { AllSvgModule } from '../../../all-svg/all-svg.module';
+import { HeaderChatOnlineComponent } from '../header-chat-online/header-chat-online.component';
 
 describe('ModalChatOnlineComponent', () => {
   let component: ModalChatOnlineComponent;
@@ -8,16 +13,50 @@ describe('ModalChatOnlineComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ModalChatOnlineComponent]
+      imports: [AllSvgModule, HttpClientTestingModule, RouterTestingModule],
+      declarations: [ModalChatOnlineComponent, HeaderChatOnlineComponent]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(ModalChatOnlineComponent);
     component = fixture.componentInstance;
+
+    component.getContainerModalChatOnline = jasmine.createSpy('getContainerModalChatOnline');
+    component.containerChatOnline = new ElementRef(document.createElement('div'));
+    component.containerChatOnlineMinimize = new ElementRef(document.createElement('div'));
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render span all join', () => {
+    const spans = fixture.nativeElement.querySelectorAll('.span-all-join');
+
+    expect(spans.length).toBe(6);
+
+    const spanElements = ["Você é um(a)...", "Primeiro Nome", "Sobrenome", "Email", "CPF", "Telefone"];
+
+    for (let i = 0; i < spans.length; i++) {
+      const element = spans[i];
+      expect(element.textContent.trim()).toBe(spanElements[i]);
+    }
+  });
+
+  it('should render select', () => {
+    const select = fixture.nativeElement.querySelector('.container-you-are-client-or-shop > select');
+    expect(select).not.toBeNull();
+  });
+
+  it('should render all input', () => {
+    const inputs = fixture.nativeElement.querySelectorAll('.input-all');
+    expect(inputs.length).toBe(5);
+  });
+
+  it('should render button start', () => {
+    const button = fixture.nativeElement.querySelector('.container-button-start-the-chat > button');
+    expect(button.textContent.trim()).toBe("INICIAR O CHAT");
   });
 });
